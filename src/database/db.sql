fda47e2dc04708af585803119ec224b24ca2e782
jorgetleite <jorgetleite@gmail.com>
2022-11-26 09:32:49 -0300

    confirma

---
+++ src/database/db.sql
@@ -0,0 +1,25 @@
+''' ESTE SCRIPT FOI RODADO NO PHPMYADMIN PARA CRIAR O BANCO DE DADOS
+AQUI ELE NÃO TEM FUNCIONALIDADE'''
+
+
+CREATE TABLE IF NOT EXISTS produtos(
+    id INT (4) UNSIGNED NOT NULL AUTO_INCREMENT,
+    fornecedor CHAR(50) NOT NULL,
+    produto CHAR(50) NOT NULL,
+    endereco CHAR(100) NULL,
+    whatsapp CHAR (50) NOT NULL,
+    email CHAR(50) NULL
+);
+
+'''
+
+CREATE TABLE IF NOT EXISTS categories(
+    id tinyint NOT NULL,
+    name CHAR(40) NOT NULL,
+    description VARCHAR(200),
+    PRIMARY KEY('id')
+);
+
+ALTER TABLE produtos ADD FOREIGN KEY ('id_category')REFERENCES categories('id');
+
+'''
